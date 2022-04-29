1e0d5e3749399b9d9f8769033891f219739ecdb8
Raj Srikar <rajsrikar.d161@gmail.com>
2022-04-29 13:46:08 +0530

    Updated to Embedded video view to avoid ads.
    The video will be opened in an embedded webpage to prevent ads from playing.
    Also load tested the server and fixed the server to prevent accepting links from multiple clients when requested at the exact same time.

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.get_link, name='home'),
-	path('yt-link/', views.yt_link, name="YtForm")
+	path('yt-link/', views.yt_link, name="YtForm"),
+	path('video', views.video, name='video')
  ]
--- todo/views.py
+++ todo/views.py
@@ -6,10 +6,12 @@ from . import checker
 
 import webbrowser
 import pafy
+from random import randint
 
 
 busy = False
 dur=None
+embedded = ''
 
 # Create your views here.
 def index(request):
@@ -36,6 +38,7 @@ def get_link(request):
         form = NameForm()
     
     return render(request, 'input.html', {'form': form})
+
     
 def yt_link(request):
 	global busy
@@ -43,15 +46,35 @@ def yt_link(request):
 	if not busy:
 		r=request.POST
 		link = dict(r)['yt_link'][0]
+		busy=True
 		try:
 			vid = pafy.new(link)
 			dur = vid.length
-			webbrowser.open(link)
-			busy=True
+			rcode = ''.join([chr(randint(65,91))+chr(randint(97,123))+chr(randint(48,58)) for x in range(3)])
+			webbrowser.open('http://127.0.0.1:8000/video?v='+link+'&r='+rcode)
 			checker.change_busy()
 			return render(request, 'response.html', {'response':'<h1>Thank You!</h1>'})
 		except:
+			busy=False
 			return render(request, 'response.html', {'response':'<h1>Bad URL!</h1><p>Provide a proper YouTube link</p>'})
 	else:
 		return render(request, 'response.html', {'response':'<h2>The player is busy. Please try again later!</h2>'})
-	#return render(request, 'video.html', {'link': inp})
+		
+
+def video(request):
+	r = request.GET
+	link = dict(r)['v'][0]
+	embedded = embed(link)
+	return render(request, 'video.html', {'link': embedded})
+
+
+
+
+def embed(link):
+    link = link.replace('?t', '?start')
+    link = link.replace('&t','?start')
+    link = link.replace('m.','')
+    embeded = link.replace('watch?v=','embed/')
+    if link == embeded: embeded = link.replace('youtu.be', 'youtube.com/embed')
+    embeded = embeded + '&' if '?start' in embeded else embeded+'?'
+    return embeded
